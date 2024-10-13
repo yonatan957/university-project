a161d712f937d896bcd63ceddf2ff58759d929ae
yonatan957 <yonatan22660@gmail.com>
2024-10-13 15:21:57 +0300

    add the auth middleware on teacher router

--- src/routes/teacherRoutes.ts
+++ src/routes/teacherRoutes.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import { addGrade, avg, changeGrade, getGrade, register, students } from "../controllers/teacherController";
+import { Teachers } from "../middlewares/authMiddlewar";
 const router = express.Router();
 
 router.post('/register', register);
 
 //expecting for description for test in body
-router.put('/addGrade/:studentId', addGrade);
+router.put('/addGrade/:studentId',Teachers, addGrade);
 
-router.put('/changeGrade/:studentId', changeGrade);
+router.put('/changeGrade/:studentId',Teachers , changeGrade);
 
-router.get('/students', students);
+router.get('/students',Teachers , students);
 
-router.get('/avg', avg);
+router.get('/avg',Teachers , avg);
 
-router.get('/grade/:studentId', getGrade);
+router.get('/grade/:studentId',Teachers , getGrade);
 export default router;
